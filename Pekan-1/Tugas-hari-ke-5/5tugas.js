b1c3681978cbc4d25de95aec4ccae926b3cab0b9
Bintang kalahari <bintangkalahari07@gmail.com>
2020-07-24 23:51:31 +0700

    yang bener mas function js

---
+++ Pekan-1/Tugas-hari-ke-5/5tugas.js
@@ -0,0 +1,33 @@
+//soal 
+function halo (){
+    return "Halo Sanbers!" 
+  }
+    var kalimat = halo ();
+        console.log("NOMOR 1")
+        console.log(halo())
+
+//soal2
+var num1 = 12;
+var num2 = 4;
+        console.log("NOMOR 2")
+
+function kalikan(num1, num2){
+  return num1*num2
+}
+var hasilkali = kalikan(num1, num2);
+        console.log(hasilkali) 
+
+//soal3
+console.log("NOMOR 5")
+var name = "John"
+var age = 30
+var address = "Jalan belum jadi"
+var hobby = "Gaming"
+var perkenalan = "Nama saya"+ " " + name + " umur saya " + " " + age + " alamat saya" + " " + address + " " + " saya punya hobby yaitu " + hobby
+
+function introduce(name, age, address, hobby){
+  return perkenalan
+}
+ 
+var perkenalan = introduce(name, age, address, hobby)
+console.log(perkenalan) // Menampilkan "Nama saya John, umur saya 30 tahun, alamat saya di jalan belum jadi, dan saya punya hobby yaitu Gaming!"

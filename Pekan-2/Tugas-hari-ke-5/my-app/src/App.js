cd925ac0540561b702a50a50a9acd9af3903e004
Bintang kalahari <bintangkalahari07@gmail.com>
2020-08-03 00:48:00 +0700

    reactpemula

---
+++ Pekan-2/Tugas-hari-ke-5/my-app/src/App.js
@@ -0,0 +1,80 @@
+//import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Bismillah</title>
+    </head>
+
+    <body  style="padding: 10px; border: 1px solid #ccc"></body>
+        
+
+        <br><br>
+
+        <div>
+            <h1 style="text-align:center"><b>Form Pembelian Buah</b></h1>
+        <div>
+        <div style="display: block; margin-bottom: 1em;">
+          <div style="display: inline-block; width: 150px; font-weight: bold; font-size: 16px;">
+            Nama Pelanggan 
+        </div>
+        <input style="display: inline-block;" type="text" name="name">
+        </div>
+            <form>
+            
+           
+            <br>
+            <br>
+            
+            <label for="nama_user"><b>Daftar Item</b></label><br>
+          
+                <div style="display: inline-block;">
+                 <input type="checkbox"> Semangka <br>
+                    <input type="checkbox"> Jeruk<br> 
+                        <input type="checkbox"> Salak<br>
+                         <input type="checkbox"> Anggur<br>
+                    </div>
+                    <br>
+           
+           
+            <br>
+            <button>Kirim</button>
+
+
+
+
+
+
+
+            </form>
+
+
+        </div>
+        
+
+    </body>
